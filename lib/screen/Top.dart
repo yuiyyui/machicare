424effc237ef95844e558acfbaa1d8521a1645bc
yuiyui <164984141+yuiyyui@users.noreply.github.com>
2024-11-20 17:03:21 +0900

    feat: 地図の表示完了

--- lib/screen/Top.dart
+++ lib/screen/Top.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import '../components/menu_drawer.dart';
 
 class TopPage extends StatefulWidget {
@@ -22,11 +24,36 @@ class _TopPageState extends State<TopPage> {
           });
         },
       ),
-      body: Center(
-        child: Text(
-          _selectedCity.isEmpty ? 'No city selected' : _selectedCity,
-          style: TextStyle(fontSize: 24),
-        ),
+      body: Stack(
+        children: [
+          // 地図を表示
+          FlutterMap(
+            options: MapOptions(
+              center: LatLng(35.681, 139.767), // 東京駅の座標
+              zoom: 14.0,
+              interactiveFlags: InteractiveFlag.all,
+            ),
+            children: [
+              // タイルレイヤー
+              TileLayer(
+                urlTemplate: "https://tile.openstreetmap.jp/{z}/{x}/{y}.png",
+              ),
+              // サークルレイヤー
+              CircleLayer(
+                circles: [
+                  CircleMarker(
+                    point: LatLng(35.681, 139.767), // 東京駅の座標
+                    color: Colors.indigo.withOpacity(0.9),
+                    radius: 10,
+                    borderColor: Colors.white.withOpacity(0.9),
+                    borderStrokeWidth: 3,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          // 選択された都市を表示するメニュー
+        ],
       ),
     );
   }
